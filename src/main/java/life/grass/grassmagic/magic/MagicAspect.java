470af624b5e1ba8ee3e00bf7ef18da7b616422cb
unicroak <unicroak@gmail.com>
2017-09-05 06:24:14 +0900

    [improve] revive readability

--- src/main/java/life/grass/grassmagic/magic/MagicAspect.java
+++ src/main/java/life/grass/grassmagic/magic/MagicAspect.java
@@ -1,58 +1,57 @@
 package life.grass.grassmagic.magic;
 
-import life.grass.grassmagic.util.Finder;
+import life.grass.grasscombat.datatype.DamageType;
+import life.grass.grasscombat.entity.Victim;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.World;
 import org.bukkit.entity.LivingEntity;
 
-import java.util.List;
-import java.util.function.Consumer;
-
 public enum MagicAspect {
-    FIRE(false, true, 1.8, (location -> {
-        World world = location.getWorld();
-
-        world.spawnParticle(Particle.FLAME, location, 4, 0.1, 0.1, 0.1, 0);
-        if (Math.random() * 100 < 15) world.spawnParticle(Particle.LAVA, location, 1, 0, 0, 0, 0);
-    })),
-    PLASMA(false, true, 1.8, (location -> {
-        World world = location.getWorld();
-
-        world.spawnParticle(Particle.ITEM_CRACK, location, 4, 0.1, 0.1, 0.1, 0, Material.GOLD_BLOCK);
-    })) {
+    FIRE(false, true) {
         @Override
-        public List<LivingEntity> spark(Location location) {
-            List<LivingEntity> entityList = super.spark(location);
+        public void exert(LivingEntity damager, LivingEntity entity) {
+            Victim victim = new Victim(entity);
 
-            if (Math.random() * 100 < 8) {
-                World world = location.getWorld();
-                Location point = location.clone().add(Math.random() * 10 - 5, Math.random() * 10 - 5, Math.random() * 10 - 5);
+            victim.causeDamage(3, damager, DamageType.MAGIC_DAMAGE);
+            entity.setFireTicks(30);
+        }
 
-                world.strikeLightningEffect(point);
-                entityList.addAll(Finder.findNearbyLivingEntities(point, 3));
-            }
+        @Override
+        public void spark(Location location) {
+            World world = location.getWorld();
 
-            return entityList;
+            world.spawnParticle(Particle.FLAME, location, 2, 0.1, 0.1, 0.1, 0);
+        }
+    },
+    PLASMA(false, true) {
+        @Override
+        public void exert(LivingEntity damager, LivingEntity entity) {
+            Victim victim = new Victim(entity);
+
+            victim.causeDamage(5, damager, DamageType.MAGIC_DAMAGE);
+        }
+
+        @Override
+        public void spark(Location location) {
+            World world = location.getWorld();
+
+            System.out.println(Particle.ITEM_CRACK.getDataType());
+            world.spawnParticle(Particle.ITEM_CRACK, location, 3, 0.1, 0.1, 0.1, 0, Material.DIAMOND_BLOCK);
         }
     };
 
     private boolean canExertAlly, canExertEnemy;
-    private double exertableRange;
-    private Consumer<Location> sparkConsumer;
 
-    MagicAspect(boolean canExertAlly, boolean canExertEnemy, double exertableRange, Consumer<Location> sparkConsumer) {
+    MagicAspect(boolean canExertAlly, boolean canExertEnemy) {
         this.canExertAlly = canExertAlly;
         this.canExertEnemy = canExertEnemy;
-        this.exertableRange = exertableRange;
-        this.sparkConsumer = sparkConsumer;
     }
 
-    public List<LivingEntity> spark(Location location) {
-        sparkConsumer.accept(location);
-        return Finder.findNearbyLivingEntities(location, exertableRange);
-    }
+    public abstract void exert(LivingEntity damager, LivingEntity entity);
+
+    public abstract void spark(Location location);
 
     public boolean canExertAlly() {
         return canExertAlly;
